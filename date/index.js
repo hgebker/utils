8a5d582965b40eb4d854dc6b9f452f5a94dfb752
Gebker Hannes <hannes.gebker@d-velop.de>
2021-05-20 13:49:26 +0200

    Add more date utils functions

--- date/index.js
+++ date/index.js
@@ -7,3 +7,19 @@ import format from 'date-fns/format';
 export function dateToString(date) {
 	return format(new Date(date), 'yyyy/MM/dd HH:mm:ss');
 }
+
+export const dateIsEqual = (date, dateToCompare) => {
+	return new Date(date).getTime() === new Date(dateToCompare).getTime();
+};
+
+export const dateIsAfter = (date, dateToCompare) => {
+	return new Date(date).getTime() > new Date(dateToCompare).getTime();
+};
+
+export const dateIsBefore = (date, dateToCompare) => {
+	return new Date(date).getTime() < new Date(dateToCompare).getTime();
+};
+
+export const dateIsWithinInterval = (date, interval) => {
+	return dateIsAfter(date, interval.start) && dateIsBefore(date, interval.end);
+};
